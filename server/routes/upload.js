7273525e24b6b972ad811633795da9af05779212
Dany Xiloj <edxiloj25huinac@gmail.com>
2019-06-20 17:16:29 -0600

    Renombrando archivos

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -2,12 +2,18 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const app = express();
 
+const Usuario = require('../models/usuario');
+
 
 // default options
 app.use(fileUpload());
 
 
-app.put('/upload', function(req, res) {
+app.put('/upload/:tipo/:id', function(req, res) {
+
+    let tipo = req.params.tipo;
+    let id = req.params.id;
+
 
     if (Object.keys(req.files).length == 0) {
         return res.status(400).json({
@@ -17,6 +23,19 @@ app.put('/upload', function(req, res) {
             }
         });
     }
+
+    let tiposValidos = ['productos', 'usuarios'];
+
+    if(tiposValidos.indexOf(tipo) <0 ){
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'El tipo no es permitido: ' + tiposValidos.join(","),
+            }
+        });
+    }
+
+
 
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
     let archivo = req.files.archivo;
@@ -37,8 +56,12 @@ app.put('/upload', function(req, res) {
             }
         });
     }
+
+    // cambiar nombre al archivo 
+    let nombreArchvio = `${id}-${ new Date().getMilliseconds() }-.${extension}`;
+
     // Use the mv() method to place the file somewhere on your server
-    archivo.mv(`uploads/${archivo.name}`, (err) => {
+    archivo.mv(`uploads/${tipo}/${nombreArchvio}`, (err) => {
         if (err)
             return res.status(500).json({
                 ok: false,
