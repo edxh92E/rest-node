3f1c563a984bd0139e1c02d67fb2bf6925b69818
Dany Xiloj <edxiloj25huinac@gmail.com>
2019-06-20 13:44:38 -0600

    Agregando autenticacion con google

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 const bodyParser = require('body-parser')
@@ -9,6 +10,8 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
+// Habilitar la carpeta public para que se pueda acceder desde cualquier lugar
+app.use( express.static( path.resolve(__dirname,'../public')));
 // Configuracion global de rutas
 app.use(require('./routes/index'));
 
