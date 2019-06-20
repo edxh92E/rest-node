baa90a267a4483c7dbec756a0fc397e7e77a5423
Dany Xiloj <edxiloj25huinac@gmail.com>
2019-06-20 11:24:40 -0600

    Variables de entorno en heroku

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://edxh92:zDVdepDQ5FRaktkB@cluster0-9pzq9.mongodb.net/test';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
