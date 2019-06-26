a38df264d988fa875fe13385068537b11b52cffa
Dany Xiloj <edxiloj25huinac@gmail.com>
2019-06-25 22:07:17 -0600

    Devolviendo imagenes

---
+++ server/routes/imagenes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+let app = express();
+
+app.get('/imagen/:tipo/:img', (req, res) => {
+    
+    let tipo = req.params.tipo;
+    let img = req.params.img;
+    let pathImg = `./uploads/${tipo}/${img}`;
+    // se envia un archivo
+    let pathNoImg = path.resolve(__dirname, '../assets/no-image.jpg');
+    res.sendFile(pathNoImg);
+
+});
+
+module.exports  = app;
